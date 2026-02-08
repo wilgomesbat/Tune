6a90f00b3afb4c7025ec5e827044505b6c6ed520
wilgomesbat <00001064295654sp@al.educacao.sp.gov.br>
2026-02-08 04:09:14 -0300

    Update highlighted music ID in main.js

--- js/main.js
+++ js/main.js
@@ -3040,7 +3040,7 @@ function createDefaultCard(item) {
     return div;
 }
 // Substitua pelo ID da MÚSICA que você quer destacar
-const MUSICA_DESTAQUE_ID = "6jRbEpPkjRoSpYtiVkKE"; 
+const MUSICA_DESTAQUE_ID = "AP76eYi80GMBzZMsivyI"; 
 
 async function loadBannerAlbum() {
     const banner = document.getElementById('new-release-banner');
