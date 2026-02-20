f0aa13a707c2069fbc742595298e97a12c017d9e
wilgomesbat <00001064295654sp@al.educacao.sp.gov.br>
2026-02-20 15:48:17 -0300

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD4gKKJh59ljwOe0PDYaJSsfEp_7PMBD8s",
+    authDomain: "tune-8cafb.firebaseapp.com",
+    projectId: "tune-8cafb",
+    storageBucket: "tune-8cafb.appspot.com",
+    messagingSenderId: "599729070480",
+    appId: "1:599729070480:web:4b2a7d806a8b7732c39315"
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+export const db = getFirestore(app);
+export const auth = getAuth(app);
